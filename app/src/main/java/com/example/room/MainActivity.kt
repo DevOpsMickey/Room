95a3a00242a67a6c1349deb23fa251a6570c5847
Mickeyfeick <68301522+Mickeyfeick@users.noreply.github.com>
2020-12-06 16:45:40 -0600

    Initial commit

---
+++ app/src/main/java/com/example/room/MainActivity.kt
@@ -0,0 +1,66 @@
+package com.example.room
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
+import androidx.cardview.widget.CardView
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
+import com.example.room.adapter.NotasAdapter
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import kotlinx.coroutines.launch
+
+
+class MainActivity : AppCompatActivity() {
+    
+    var app:NotaApp?=null
+    var myRecyclerNotas:RecyclerView?=null
+    var fadAdd:FloatingActionButton?=null
+    var notas:ArrayList<Nota>?=null
+    var adapter:NotasAdapter?=null
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        app = applicationContext as NotaApp
+
+        fadAdd = findViewById(R.id.fabAddNota)
+        myRecyclerNotas = findViewById(R.id.myRecyclerNotas)
+
+
+        notas = ArrayList<Nota>()
+
+
+        lifecycleScope.launch {
+            val notasdb = app!!.db.notaDAO().getAll()
+            notas!!.addAll(notasdb)
+            adapter!!.notifyDataSetChanged()
+        }
+
+        adapter = NotasAdapter(notas!!, this)
+
+        fadAdd!!.setOnClickListener {
+            val i = Intent(this, Add_Notas::class.java)
+            startActivity(i)
+        }
+
+
+        myRecyclerNotas!!.layoutManager = StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL)
+        myRecyclerNotas!!.setHasFixedSize(true)
+        myRecyclerNotas!!.adapter = adapter
+
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+        lifecycleScope.launch {
+            adapter!!.notifyDataSetChanged()
+        }
+    }
+}
